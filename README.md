8dac0ad1999c7b6fcaaf12694a73519b53135e92
Stefan Emanuelsson <semanuelsson@gmail.com>
2018-03-12 19:08:36 +0100

    Version 2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Servlet 3.0 examples.
 
-## Ver_1 
-A basic Hello World! servlet and jsp, with xml descriptor.
+## Version 1 
+A basic Hello World! servlet and jsp, with xml descriptor. Tag: v1
+
+## Version 2
+Same basic hello world, now with annotation descriptor on the servlet. Tag: v2
--- src/main/java/com/demo/webapp/HelloServlet.java
+++ src/main/java/com/demo/webapp/HelloServlet.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet( name="Hello-Servlet", urlPatterns={"/hello"} )
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
